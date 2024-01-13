afabd91e9d100fd6f1a7e4141bee05ea7ca971f3
andyfullstack <andriikoliadenko@gmail.com>
2024-01-13 21:15:31 +0200

    upd

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,27 +1,27 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { Overlay, ModalDiv, ModalImg } from './Modal.styled.jsx';
 
-class Modal extends Component {
-  handleEsc = e => {
-    if (e.code === 'Escape') this.props.onHideModal();
-    console.log('esc');
-  };
-  componentDidMount() {
-    document.addEventListener('keydown', this.handleEsc);
-  }
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.handleEsc);
-  }
-  render() {
-    const { largeImageURL } = this.props.imageData;
-    return (
-      <Overlay onClick={this.props.onHideModal}>
-        <ModalDiv>
-          <ModalImg src={largeImageURL} alt="largeImage" />
-        </ModalDiv>
-      </Overlay>
-    );
-  }
-}
+const Modal = ({ onHideModal, imageData }) => {
+  useEffect(() => {
+    const handleEsc = e => {
+      if (e.code === 'Escape') onHideModal();
+    };
+
+    document.addEventListener('keydown', handleEsc);
+
+    return () => {
+      document.removeEventListener('keydown', handleEsc);
+    };
+  }, [onHideModal]);
+
+  const { largeImageURL } = imageData;
+  return (
+    <Overlay onClick={onHideModal}>
+      <ModalDiv>
+        <ModalImg src={largeImageURL} alt="largeImage" />
+      </ModalDiv>
+    </Overlay>
+  );
+};
 
 export default Modal;
